aa08d8d24a8dbf8e8f9f66c146afb6abb98edef7
Wei <wei.xw.xu@gmail.com>
2020-01-27 22:56:48 -0500

    added jwt

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,9 @@ let Household = require('../models/household.model');
 let User = require('../models/user.model');
 
 let bcrypt = require('bcryptjs');
+let jwt = require('jsonwebtoken');
+
+let auth = require('./verifyToken');
 
 router.get('/', function(req, res, next){
     let id = req.body.id;
@@ -22,7 +25,6 @@ router.get('/', function(req, res, next){
 router.post('/register', function(req, res, next){
     let { email, username, password } = req.body;
     // console.log(email, username, password);
-    console.log(email);
 
     // check if email has already been used
     User.findOne({email}).then(user => {
@@ -37,49 +39,37 @@ router.post('/register', function(req, res, next){
         if(user){
             console.log("Username already registered.");
             return res.json({msg:"Username already taken."});
+        }else{
+            // encrypting password
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(password, salt, (err, hash) => {
+                    if (err) throw err;
+                    let password = hash;
+                    console.log(password);
+
+                    let newUser = new User({
+                        username,
+                        email,
+                        password
+                    });
+
+                    //save to db
+                    newUser.save().then(() => {
+                        console.log(`New user ${username} registered successfully.`);
+                        // jwt send token
+                        let token = jwt.sign({_id:user._id}, process.env.TOKEN_SECRET, { expiresIn: 100000});
+                        res.header('auth-token', token).send(token);
+
+
+
+
+                        return res.json({msg:`New user ${username} registered successfully.`});
+                    }).catch(err => console.log(err));
+
+                });
+            });
         }
     })
-
-    // encrypting password
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(password, salt, (err, hash) => {
-            if (err) throw err;
-            password = hash;
-            console.log(password);
-        });
-    });
-
-
-    let newUser = new User({
-        username,
-        email,
-        password
-    });
-
-
-    //save to db
-    newUser.save().then(() => {
-        console.log(`New user ${username} registered successfully.`);
-        // jwt send token
-        return res.json({msg:`New user ${username} registered successfully.`});
-    }).catch(err => console.log(err));
-
-    // // encrypting password then save to db
-    // bcrypt.genSalt(10, (err, salt) => {
-    //     bcrypt.hash(password, salt, (err, hash) => {
-    //         if (err) throw err;
-    //         newUser.password = hash;
-    //         console.log(newUser.password);
-    //         newUser.save().then(() => {
-    //             console.log(`New user ${username} registered successfully.`);
-
-    //             // jwt send token
-    //             return res.json({msg:`New user ${username} registered successfully.`});
-    //         }).catch(err => console.log(err));
-
-    //     });
-    // });
-
     
 })
 
@@ -90,17 +80,32 @@ router.post('/login', function(req, res, next){
 
     User.findOne({email}).then(user => {
         if(user){
-            
             // sub for bcrypt when using it
-            if(user.password === password){
-                // send header
-            }
+            bcrypt.compare(password, user.password, function(err, success){
+                if(err) throw err;
+                if(success){
+                    // send header
+                    let token = jwt.sign({_id:user._id}, process.env.TOKEN_SECRET, { expiresIn: 100000});
+                    return res.header('auth-token', token).send(token);
 
+                    // console.log(`User ${user.username} Log in successfully`);
+                    // return res.json({msg:`User ${user.username} Log in successfully`});
+                }else{
+                    console.log(`Wrong password for ${user.username}`);
+                    return res.json({msg:`Wrong password for ${user.username}`});
+                }
+            });
         }else{
             console.log("Email does not exist");
             return res.json({msg:"Email does not exist"});
         }
     }).catch(err => console.log(err));
 });
+
+
+router.post('/testToken', auth, function(req, res, next){
+    console.log(`token is`);
+    return res.json({msg:`Token validated, user is ${req.user._id}`})
+})
 
 module.exports = router;
---
+++ routes/verifyToken.js
@@ -0,0 +1,19 @@
+let jwt = require('jsonwebtoken');
+
+module.exports = function(req, res, next){
+    const token = req.header('auth-token');
+    if(!token){
+        return res.status(401).send('Access Denied');
+    }else{
+        try{
+            const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+            req.user = verified;
+            console.log(req.user);
+            next();
+        }catch(err){
+            return res.status(400).send('Invalid Token');
+        }
+       
+    }
+}
+
